ee92bfcf3e3d06c5fb2fe1a788dd4b015204a5ad
seheonnn <ho78901@naver.com>
2023-10-31 12:19:05 +0900

    [10.31] CRUD

--- src/main/java/com/example/springc/repository/UserRepository.java
+++ src/main/java/com/example/springc/repository/UserRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 
 // JPA 상속
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+    // JPA 에서 기본적으로 제공해 주는 findBy~ 메소드들은 모두 return 값이 Optional
+    // Optional 과 일반 Object 의 차이는 Optional 에는 null 값이 들어갈 수 있고 일반 Object 는 null 값 불가능하므로 오류 발생
+    Optional<UserEntity> findByEmail(String email);
 }

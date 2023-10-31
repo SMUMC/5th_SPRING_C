ea635335776d9be5f38a152928e9856a4e10daab
seheonnn <ho78901@naver.com>
2023-10-31 12:19:40 +0900

    [10.31] RoleType update

--- src/main/java/com/example/springc/config/RoleType.java
+++ src/main/java/com/example/springc/config/RoleType.java
@@ -1,8 +1,21 @@
 package com.example.springc.config;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Getter
+@RequiredArgsConstructor
 public enum RoleType {
-    USER, MANAGER
+
+    USER(0, "USER"),
+    MANAGER(1, "MANAGER");
+
+    private final Integer value;
+    private final String role;
+
+
+    @JsonValue
+    public String getRole() {
+        return role;
+    }
 }
